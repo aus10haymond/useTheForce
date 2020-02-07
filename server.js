bcda6916f07fc1fb766ac407029faba6b8e9540b
Austen Haymond <ahaymon17@gmail.com>
2020-02-06 19:03:17 -0700

    made small change

--- server.js
+++ server.js
@@ -43,11 +43,11 @@ const characters = [
 
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "view.html"));
+  res.sendFile(path.join(__dirname, "/view.html"));
 });
 
 app.get("/add", function(req, res) {
-  res.sendFile(path.join(__dirname, "add.html"));
+  res.sendFile(path.join(__dirname, "/add.html"));
 });
 
 // Displays all characters
